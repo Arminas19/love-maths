f0e23a18bfec103d383e2a58b4949fe1ec28e0b6
Arminas19 <armeska14@gmail.com>
2021-10-01 11:32:19 +0000

    display addition questions to the user

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -5,29 +5,36 @@ document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
     for(button of buttons){
 
-        button.addEventListener('click', function(){
-            if(this.getAttribute('data-type') === 'submit'     ){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit"){
                     alert('You clicked Submit');
             }
             else{
-                let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
             }
 
         });
     }
+     runGame("addition");
 });
+
 
 /**
  * The main game "loop", called when the Script is first loaded
  * and after the users answer has been processed.
  * 
  */
-function runGame() {
-    //creates two random numbers between 1 - 25
- let num1 = Math.floor(Math.random() * 25 + 1);
- let num2 = Math.floor(Math.random() * 25 + 1);
-
+ function runGame(gameType) {
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -47,7 +54,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+document.getElementById("operand1").textContent = operand1;
+document.getElementById("operand2").textContent = operand2;
+document.getElementById("operator").textContent = "+";
 
 }
 

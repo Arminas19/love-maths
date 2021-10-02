d7fcc8c8309a85be1827cfa0ce8ecbbbfb36f593
Arminas19 <armeska14@gmail.com>
2021-10-02 11:16:00 +0000

    add multiply game code

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -31,7 +31,9 @@ document.addEventListener("DOMContentLoaded", function(){
     let num2 = Math.floor(Math.random() * 25) + 1;
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if(gameType === "multiply"){
+displayMultiplyQuestion(num1, num2);
+    }else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -72,6 +74,8 @@ let operator = document.getElementById('operator').innerText;
 if(operator === "+"){
     return [operand1 + operand2, "addition"];
     
+    }else if(operator === "x"){
+return[operand1 * operand2, "multiply"];
     }else{
     alert(`Unimplemented Error ${operator}`);
     throw`unimplemented Error ${operator}.Aborting!`;
@@ -108,8 +112,11 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
+    
 } 
 
 

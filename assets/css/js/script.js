584d7799d3e5d2719e1c9e6ca4efe2de32d87533
Arminas19 <armeska14@gmail.com>
2021-10-02 11:38:54 +0000

    add subtraction game code

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -33,7 +33,9 @@ document.addEventListener("DOMContentLoaded", function(){
         displayAdditionQuestion(num1, num2);
     } else if(gameType === "multiply"){
 displayMultiplyQuestion(num1, num2);
-    }else {
+    } else if(gameType === "subtract"){
+displaySubtractQuestion(num1, num2);
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -76,6 +78,8 @@ if(operator === "+"){
     
     }else if(operator === "x"){
 return[operand1 * operand2, "multiply"];
+    } else if(operator === "-"){
+return[operand1 - operand2, "subtract"];
     }else{
     alert(`Unimplemented Error ${operator}`);
     throw`unimplemented Error ${operator}.Aborting!`;
@@ -108,8 +112,10 @@ document.getElementById("operator").textContent = "+";
 
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+document.getElementById("operand1").textContent = operand1 > operand2 ? operand1: operand2;
+document.getElementById("operand2").textContent = operand1 > operand2 ? operand2: operand1;
+document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
